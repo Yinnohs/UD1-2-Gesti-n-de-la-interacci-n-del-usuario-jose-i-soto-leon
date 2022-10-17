543d597676bb2f48ab81ac90c6c0eb648b3f29c3
josesoto <josesoto@itop.es>
2022-10-17 22:00:31 +0100

    feat(Main): Created main component as app access point

---
+++ Main.js
@@ -0,0 +1,17 @@
+import { AppRegistry } from 'react-native'
+import {PaperProvider} from 'react-native-paper'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import App from './App'
+
+
+export default function Main (){
+    return(
+        <SafeAreaProvider>
+            <PaperProvider>
+                <App/>
+            </PaperProvider>
+        </SafeAreaProvider>
+    )
+}
+
+AppRegistry.registerComponent('shopping-list', ()=> Main)
