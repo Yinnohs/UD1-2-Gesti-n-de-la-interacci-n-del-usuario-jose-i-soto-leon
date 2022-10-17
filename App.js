4f1fadec8532533caa56f6ea53791257f51cad11
josesoto <josesoto@itop.es>
2022-10-17 20:33:04 +0100

    feat(all):created Navigation

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
+import React  from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
+
+  const Drawer = createDrawerNavigator()
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Drawer.Navigator>
+        <Drawer.Screen name="About" component={<View></View>} />
+        <Drawer.Screen name="ShoppingList" component={<View></View>} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+      <StatusBar style='auto'/>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
