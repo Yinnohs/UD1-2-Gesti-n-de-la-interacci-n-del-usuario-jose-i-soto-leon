b557ed6e8b798c93408a0000848502016324563e
josesoto <josesoto@itop.es>
2022-10-21 11:26:06 +0100

    feat(colors):created app theme

---
+++ colors/colors.js
@@ -0,0 +1,9 @@
+export const theme={
+    primaryColor: "#bf358f",
+    secondaryColor: "#93296c",
+    terciaryColor: "#85196c",
+    backgroundColor:"#313742",
+    shadowBorderColor:"#444752",
+    inputBackground: "#181818",
+    backgroundViewColor: "#282C34"
+}
